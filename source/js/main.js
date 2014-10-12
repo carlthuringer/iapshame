60bb287b69763522332c4fb142d8d5716d67d99e
Carl Thuringer <carlthuringer@gmail.com>
2014-10-12 14:19:02 -0500

    Search from all pages with some smartness and crappy hashquery

--- source/js/main.js
+++ source/js/main.js
@@ -2,24 +2,37 @@
 
 (function() {
   angular.module('iapShame', [])
-  .run(function($rootScope) {
-    $rootScope.search = {
-      term: ''
-    }
-  })
-  .controller("SearchController", function($scope) {
-    $scope.search = {
-      term: ''
-    };
+  .factory('search', ['$location', function($location) {
+    return { term: $location.search().search } }
+  ])
+  .controller("SearchController", [
+    '$scope', '$rootScope', '$location', '$window', 'search',
+    function($scope, $rootScope, $location, $window, search) {
+      $scope.search = search
 
-    $scope.searching = function() {
-      $scope.$root.search = $scope.search
+      $scope.searching = function() {
+        search = $scope.search
+      }
+
+      $scope.searchSubmit = function() {
+        if($location.url().match(/all_games/)) return void 0;
+
+        $window.location = "all_games.html#?search=" + search.term
+      }
     }
+  ])
+  .controller("AllGamesController", ['$scope', 'search', function($scope, search) {
+    $scope.search = search
 
     $scope.hideWhenSearching = function(game_name) {
-      var term = $scope.$root.search.term;
-      if($scope.$root.search.term == '') return false
-      var expression = new RegExp($scope.$root.search.term, "i");
+      var expression
+      if(search.term == '') return false;
+      try {
+        expression = new RegExp(search.term, "i");
+      } catch(e) {
+        return false;
+      }
+
 
       if(game_name.match(expression)) {
         return false
@@ -27,6 +40,6 @@
         return true
       }
     }
-  })
+  }])
 })();
 

f91e3dfd44dc6ff265dbdeeaef4436de4d56701e
Carl Thuringer <carl@groupon.com>
2015-03-28 08:07:54 -0500

    Add pagination to all games page

--- source/js/main.js
+++ source/js/main.js
@@ -1,32 +1,33 @@
-'use strict';
-
 (function() {
-  angular.module('iapShame', [])
+  'use strict';
+  angular.module('iapShame', ['angularUtils.directives.dirPagination'])
   .factory('search', ['$location', function($location) {
-    return { term: $location.search().search } }
+    return { term: $location.search().search }; }
   ])
   .controller("SearchController", [
     '$scope', '$rootScope', '$location', '$window', 'search',
     function($scope, $rootScope, $location, $window, search) {
-      $scope.search = search
+      $scope.search = search;
 
       $scope.searching = function() {
-        search = $scope.search
-      }
+        search = $scope.search;
+      };
 
       $scope.searchSubmit = function() {
         if($location.url().match(/all_games/)) return void 0;
 
-        $window.location = "all_games.html#?search=" + search.term
-      }
+        $window.location = "all_games.html#?search=" + search.term;
+      };
     }
   ])
-  .controller("AllGamesController", ['$scope', 'search', function($scope, search) {
-    $scope.search = search
+  .controller('AllGamesController', ['$scope', 'search', function($scope, search) {
+    $scope.search = search;
+    $scope.games = window.allGames;
+    console.log($scope.games);
 
     $scope.hideWhenSearching = function(game_name) {
-      var expression
-      if(search.term == '') return false;
+      var expression;
+      if(search.term === '') return false;
       try {
         expression = new RegExp(search.term, "i");
       } catch(e) {
@@ -35,11 +36,15 @@
 
 
       if(game_name.match(expression)) {
-        return false
+        return false;
       } else {
-        return true
+        return true;
       }
-    }
+    };
   }])
+  .config(['paginationTemplateProvider', function(paginationTemplateProvider) {
+    paginationTemplateProvider.setPath('/js/vendor/dirPagination/dirPagination.tpl.html');
+  }]);
+
 })();
 
